069c88b87bdc0e8beab446c5a7ddead9bfd635a6
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-27 12:44:52 +0300

    complited

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  def initialize word
+      @word = word
+  end
+  
+  def match words
+      words.select do |word|
+      word.split("").sort == @word.split('').sort
+      end
+  end
+end
